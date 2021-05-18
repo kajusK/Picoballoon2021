0a9ff5e9b14e5af4d6566f8b184e4249cfadc1d1
Nellia <nelaslezakova@seznam.cz>
2021-05-18 21:13:38 +0200

    added .gitignore, execute block into app.py, fixed test creating database

--- web/tests/test_app.py
+++ web/tests/test_app.py
@@ -3,19 +3,23 @@ import os
 from app import database_status, endpoint
 from db import Database
 
-db = Database()
 
-def test_database_created():
+def test_database_creation():
+    '''
+    If there is no file "database.sqlite" in current directory,
+    function database_status() creates one
+    '''
     database_status()
-    assert not os.path.exists('/../database.sqlite')
+    assert os.path.exists('database.sqlite')
+    os.remove('database.sqlite')
 
 
 @pytest.mark.parametrize('attribute', ['__cursor', '__connection'])
 def test_db_private_attributes(attribute):
+    '''Database attributes aren't accesible outside class'''
+    db = Database()
     with pytest.raises(AttributeError):
         assert db.attribute
 
-
 # To do:
-    # endpoint can receive data
-    # endpoint is able to insert data into database
+    # endpoint can receive and insert data into database
