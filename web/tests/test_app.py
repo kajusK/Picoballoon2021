e0429c354b00210b70d46edeef6d705c6c071f56
Nellia <nelaslezakova@seznam.cz>
2021-05-22 21:41:49 +0200

    removed test of database creation and added test of endpoint storing data

--- web/tests/test_app.py
+++ web/tests/test_app.py
@@ -1,16 +1,16 @@
 import pytest
-import os
 
 
-@pytest.fixture(scope="session")
-def db(tmp_path):                       # temporary path
+@pytest.fixture
+def db(tmp_path):                       # create temporary database instance
+    print(tmp_path)
     from db import Database
     db = Database(tmp_path)
     db.create_database_structure()
     yield db
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def app(db):
     from flask import current_app
     from app import app
@@ -19,21 +19,10 @@ def app(db):
     return app
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def client(app):
     with app.test_client() as client:
         yield client
-
-
-def test_database_creation(db):
-    '''
-    If there is no file "database.sqlite" in current directory,
-    function database_status() creates one
-    '''
-    from app import database_status
-    database_status(db)
-    assert os.path.exists('database.sqlite')
-    os.remove('database.sqlite')
 
 
 def test_db_private_attributes(db):
@@ -45,6 +34,7 @@ def test_db_private_attributes(db):
 
 
 def test_endpoint_returns_200(client):
+    '''Endpoint receives data and return status_code 200'''
     response = client.post('/endpoint', json={
         'timestamp': 12092021,
         'freq': 99.5,
@@ -63,3 +53,26 @@ def test_endpoint_returns_200(client):
         'json': 'all data here'
     })
     assert response.status_code == 200
+
+
+def test_endpoint_stores_data(client, db):
+    '''Endpoint inserts received data into database'''
+    client.post('/endpoint', json={
+        'timestamp': 12092021,
+        'freq': 99.5,
+        'rssi': 4,
+        'estimated_lat': 49.195061,
+        'estimated_lon': 16.606836,
+        'pressure_pa': 101,
+        'temp_c': 20.5,
+        'core_temp_c': 15.5,
+        'alt_m': 1000,
+        'lat': 49.195061,
+        'lon': 16.606836,
+        'battery_mv': 30,
+        'loop_time_s': 70000,
+        'gps_fix': 1,
+        'json': 'all data here'
+    })
+    for data_row in db.fetch_data():
+        assert len(data_row) == 15
