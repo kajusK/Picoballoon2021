fcf905dc5bcf0196e2e433e497b9e35b5d83be99
Nellia <nelaslezakova@seznam.cz>
2021-05-22 20:02:23 +0200

    added flask context and temporary path to tests

--- web/app.py
+++ web/app.py
@@ -9,26 +9,28 @@
 
 import os
 import pathlib
-from flask import Flask, request
+from flask import Flask, request, current_app, Response
 from db import Database
 
 app = Flask(__name__)
 
 def database_status(db):
     if not os.path.exists('database.sqlite'):
-        db.create_database_structure()
+        current_app.db.create_database_structure()
 
 
 @app.route('/endpoint', methods=['POST'])
 def endpoint():
     '''Insert incoming data into database'''
     data = request.get_json(force=True)
-    db.store_data(data)
-    return 200
+    current_app.db.store_data(data)
+    status_code = Response(status=200)
+    return status_code
 
 
 if __name__ == '__main__':
     path = str(pathlib.Path().absolute())
-    db = Database(path)
-    database_status(db)
+    with app.app_context():                 # management of the application context
+        current_app.db = Database(path)     # proxy to the application handling the current request
+        database_status(current_app.db)
     app.run(debug=True)
