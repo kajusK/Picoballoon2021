bb0d9c4770664adde91c8d286d3999e5648e3c85
Nellia <nelaslezakova@seznam.cz>
2021-05-22 20:29:29 +0200

    added fixtures for client and app

--- web/tests/test_app.py
+++ web/tests/test_app.py
@@ -1,15 +1,28 @@
 import pytest
 import os
-import pathlib
-from app import database_status, endpoint, app
-from db import Database
 
 
 @pytest.fixture(scope="session")
 def db(tmp_path):                       # temporary path
+    from db import Database
     db = Database(tmp_path)
     db.create_database_structure()
     yield db
+
+
+@pytest.fixture(scope="session")
+def app(db):
+    from flask import current_app
+    from app import app
+    with app.app_context():
+        current_app.db = db
+    return app
+
+
+@pytest.fixture(scope="session")
+def client(app):
+    with app.test_client() as client:
+        yield client
 
 
 def test_database_creation(db):
@@ -17,6 +30,7 @@ def test_database_creation(db):
     If there is no file "database.sqlite" in current directory,
     function database_status() creates one
     '''
+    from app import database_status
     database_status(db)
     assert os.path.exists('database.sqlite')
     os.remove('database.sqlite')
@@ -30,23 +44,22 @@ def test_db_private_attributes(db):
         assert db.__connection
 
 
-def test_endpoint_returns_200(db):
-    with app.test_client() as client:
-        response = client.post('/endpoint', json={
-            'timestamp': 12092021,
-            'freq': 99.5,
-            'rssi': 4,
-            'estimated_lat': 49.195061,
-            'estimated_lon': 16.606836,
-            'pressure_pa': 101,
-            'temp_c': 20.5,
-            'core_temp_c': 15.5,
-            'alt_m': 1000,
-            'lat': 49.195061,
-            'lon': 16.606836,
-            'battery_mv': 30,
-            'loop_time_s': 70000,
-            'gps_fix': 1,
-            'json': 'all data here'
-            })
-        assert response.status_code == 200
+def test_endpoint_returns_200(client):
+    response = client.post('/endpoint', json={
+        'timestamp': 12092021,
+        'freq': 99.5,
+        'rssi': 4,
+        'estimated_lat': 49.195061,
+        'estimated_lon': 16.606836,
+        'pressure_pa': 101,
+        'temp_c': 20.5,
+        'core_temp_c': 15.5,
+        'alt_m': 1000,
+        'lat': 49.195061,
+        'lon': 16.606836,
+        'battery_mv': 30,
+        'loop_time_s': 70000,
+        'gps_fix': 1,
+        'json': 'all data here'
+    })
+    assert response.status_code == 200
