8fe25560389d8ecefa9d8e8755fd3e7b6d61e767
Nellia <nelaslezakova@seznam.cz>
2021-06-24 14:05:06 +0200

    provide correct data to index

--- web/app.py
+++ web/app.py
@@ -13,12 +13,13 @@
             # add trajectory ✓
             # adjust size according to balloon route ✓
         # display picture ✓ --> old one!
-        # display summary table ✓
+        # display summary table ✓ ---> fixed size ✓
         # display graphs of temperature and altitude in time x
         # display basic info ✓ --> lorem for now
         # make index.html responsive ✓
         # make index.html pretty ✓
-    # if there are no data from gps, use lat, lon, alt from gateway
+    # if there are no data from gps, use lat, lon, alt from gateway ✓
+    # add security check into database
 
 import pathlib
 from datetime import datetime
@@ -38,19 +39,55 @@ def provide_data_markers():
         - card_body = information about temperature, battery, altitude, longitude and latitude
         - longitude, latitude
     '''
-    data_all = provide_data()
+    data_all = provide_data_table()
     data_markers = []
     for i, entry in enumerate(data_all):
         time, pressure, temp, alt, lat, lon, battery = entry
+        if temp == 'None':
+            temp = 'missing'
+        if battery == 'None':
+            battery = 'missing'
+        if alt == 'None':
+            alt = 'missing'
+        if lon == 'None':
+            lon = 'missing'
+        if lat == 'None':
+            lat = 'missing'
         marker_id = i
         card_body = f'temperature: {temp}, probe battery: {battery}, altitude: {alt}, longitude: {lon}, latitude: {lat}'
         data_markers.append([marker_id, time, card_body, lon, lat])
     return data_markers
 
 
+def provide_data_table():
+    '''
+    Provide data for a summary table:
+        - Time
+        - Pressure
+        - Temperature
+        - Altitude
+        - Latitude
+        - Longitude
+        - Battery
+    If there are missing data from GPS use data from gateways
+    '''
+    data_all = provide_data()
+    data_table = []
+    for entry in data_all:
+        time, pressure, temp, alt, lat, lon, battery, lat_gw, lon_gw, alt_gw = entry
+        if alt in ['None', 0]:
+            alt = alt_gw
+        if lat in ['None', 0]:
+            lat = lat_gw
+        if lon in ['None', 0]:
+            lon = lon_gw
+        data_table.append([time, pressure, temp, alt, lat, lon, battery])
+    return data_table
+
+
 def provide_data():
     '''
-    Provide data (a list of lists) (eg. for a summary table), specifically:
+    Provide data (a list of lists), specifically:
         - Time (day.month.year hour:minutes)
         - Pressure (HPa)
         - Temperature
@@ -58,25 +95,42 @@ def provide_data():
         - Latitude
         - Longitude
         - Battery (V)
+        - Latitude from gateway
+        - Longitude from gateway
+        - Altitude (m) from gateway
     If outside temperature seems to be invalid, use temperature of core.
+    If altitude is None or 0, calculate it from pressure.
     '''
     data_raw = current_app.db.fetch_all_data()
     data = []
     for entry in data_raw:
-        timestamp, pressure, temp, core_temp, alt, lat, lon, battery_mv = entry[:8]
+        timestamp, pressure, temp, core_temp, alt, lat, lon, bat_mv, loop_time, lat_gw, lon_gw, alt_gw = entry[:-1]
         time = datetime.fromtimestamp(timestamp).strftime("%d.%m.%Y %H:%M")
-        pressure = '{:.2f} HPa'.format(pressure / 100)
-        if temp < -100 or temp > 50 or temp == 0:      # if temperature is nonsense, use temperature of core
-            temp = '{:.1f} °C'.format(core_temp)
-        else:
-            temp = '{:.1f} °C'.format(temp)
-        if alt == 0:    # invalid input, calculate altitude from pressure
-            alt = round((145366.45 * (1 - pow(pressure / 101325, 0.190284))) / 3.2808)
-        alt = '{:.0f} m'.format(alt)
-        lat = '{:.3f}'.format(lat)
-        lon = '{:.3f}'.format(lon)
-        battery = '{:.3f} V'.format(battery_mv / 1000)
-        data.append([time, pressure, temp, alt, lat, lon, battery])
+        if alt == 0 or alt == 'None':    # invalid input, calculate altitude from pressure
+            if pressure != 'None':
+                alt = round((145366.45 * (1 - pow(pressure / 101325, 0.190284))) / 3.2808)
+        if pressure != 'None':
+            pressure = '{:.2f} HPa'.format(pressure / 100)
+        if temp != 'None':
+            if temp < -100 or temp > 50 or temp == 0:      # if temperature is nonsense, use temperature of core
+                temp = '{:.1f} °C'.format(core_temp)
+            else:
+                temp = '{:.1f} °C'.format(temp)
+        if alt != 'None':
+            alt = '{:.0f} m'.format(alt)
+        if lat != 'None':
+            lat = '{:.3f}'.format(lat)
+        if lon != 'None':
+            lon = '{:.3f}'.format(lon)
+        if bat_mv != 'None':
+            battery = '{:.3f} V'.format(bat_mv / 1000)
+        if lat_gw != 'None':
+            lat_gw = '{:.3f}'.format(lat_gw)
+        if lon_gw != 'None':
+            lon_gw = '{:.3f}'.format(lon_gw)
+        if alt_gw != 'None':
+            alt_gw = '{:.0f} m'.format(alt_gw)
+        data.append([time, pressure, temp, alt, lat, lon, battery, lat_gw, lon_gw, alt_gw])
     return data
 
 
@@ -88,7 +142,7 @@ def index():
         - markers and their cards
         - graphs of development of temperature and altitude
     '''
-    data_table = provide_data()
+    data_table = provide_data_table()
     data_markers = provide_data_markers()
     return render_template('index.html',
                            data_markers=data_markers,
@@ -111,4 +165,4 @@ if __name__ == '__main__':
     path = str(pathlib.Path().resolve())
     with app.app_context():                 # management of the application context
         current_app.db = Database(path)     # proxy to the application handling the current request
-    app.run(debug=True)
+    app.run(debug=True)                     # must be disabled!
